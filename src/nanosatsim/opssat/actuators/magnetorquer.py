6085cbaeffa7833dabf8e1510188ea1dac8e2c05
johnryan465 <johnryan465@gmail.com>
2022-03-20 22:40:24 +0000

    Updated actuators

--- src/nanosatsim/opssat/actuators/magnetorquer.py
+++ src/nanosatsim/opssat/actuators/magnetorquer.py
@@ -1,33 +1,28 @@
 from abc import abstractmethod
+from dataclasses import dataclass
 import math
 import numpy as np
-import numpy.typing as npt
+from numpy.typing import NDArray
 from numpy import float64
 from nanosatsim.spacecraft.actions import ActuatorAction
 from nanosatsim.spacecraft.actuators import Actuator, ActuatorState
 
 
 class SetOfMagnetorquersState(ActuatorState):
-    def __init__(self, torque: npt.NDArray[float64], magnetic_diopole: npt.NDArray[float64]) -> None:
-        self.torque = torque
+    def __init__(self, magnetic_diopole: NDArray[float64]) -> None:
         self.magnetic_diopole = magnetic_diopole
 
-    def get_magnetic_dipole(self) -> npt.NDArray[np.float64]:
+    def get_magnetic_dipole(self) -> NDArray[np.float64]:
         return self.magnetic_diopole
 
-    def get_control_torque(self) -> npt.NDArray[float64]:
-        return self.torque
-
-
+@dataclass
 class MagnetorquersAction(ActuatorAction):
-    pass
+    goal_magnetic_dipole: NDArray[np.float64]
 
 
 class SetOfMagnetorquers(Actuator[SetOfMagnetorquersState, MagnetorquersAction]):
-    def __init__(self) -> None:
-        self._state = SetOfMagnetorquersState(np.zeros(3), np.zeros(3))
-        self.max_magnetic_dipole = 30.0
-        self.mean_magnetic_field = 0.948
+    def __init__(self, state: SetOfMagnetorquersState) -> None:
+        self._state = state
 
     @property
     def state(self) -> SetOfMagnetorquersState:
@@ -37,20 +32,8 @@ class SetOfMagnetorquers(Actuator[SetOfMagnetorquersState, MagnetorquersAction])
     def state(self, state: SetOfMagnetorquersState) -> None:
         self._state = state
 
-    def actuate(self, magneticField_body: npt.NDArray[float64], t_mtr: npt.NDArray[float64]) -> SetOfMagnetorquersState:
+    def actuate(self, action: MagnetorquersAction) -> None:
+        pass
 
-        # computing the magneticDipole with cross-product-control-law
-        magneticDipole_body_Am2 = np.cross(magneticField_body, t_mtr) * (-1 / math.pow(self.mean_magnetic_field, 2))
-
-        # unit conversion to Tesla
-        magneticField_body_T = magneticField_body * (1E-9)
-
-        # check saturation in each magnetorquer
-        for i in range(0, 3):
-            if (abs(magneticDipole_body_Am2[i]) > self.max_magnetic_dipole):
-                magneticDipole_body_Am2[i] = np.sign(magneticDipole_body_Am2[i]) * self.max_magnetic_dipole
-
-        # computing the torque
-        torque_body = np.cross(magneticDipole_body_Am2, magneticField_body_T)
-        self.state = SetOfMagnetorquersState(torque_body, magneticDipole_body_Am2)
-        return self.state
+    def get_name(self) -> str:
+        return "set_of_magnetorquers"
--- src/nanosatsim/opssat/actuators/reactionwheel.py
+++ src/nanosatsim/opssat/actuators/reactionwheel.py
@@ -1,43 +1,27 @@
 
-import imp
+from dataclasses import dataclass
 import math
 from nanosatsim.spacecraft.actions import ActuatorAction
 from nanosatsim.spacecraft.actuators import Actuator, ActuatorState
-import numpy.typing as npt
+from numpy.typing import NDArray
 import numpy as np
-from numpy import float64, floating
 
 
 class SetOfReactionWheelsState(ActuatorState):
-    def __init__(self, control_torque: npt.NDArray[np.float64], angular_velocity: npt.NDArray[np.float64], inertia: float) -> None:
-        self.control_torque = control_torque
-        self.angular_velocity = angular_velocity
-        self.inertia = inertia
+    def __init__(self, angular_velocity: NDArray[np.float64]) -> None:
+        self._angular_velocity = angular_velocity
 
-    def get_angular_momentum_norm(self, x: npt.NDArray[np.float64]) -> floating:
-        return np.linalg.norm(self.get_angular_momentum(x))
+    def get_angular_velocity(self) -> NDArray[np.float64]:
+        return self._angular_velocity
 
-    def get_angular_momentum(self, x: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
-        # h_{w,n} = I_{n,s}a_n^T\omega + I_{n,s}\omega_n
-        return (self.angular_velocity + x) * self.inertia
-
-    def get_angular_velocity(self) -> npt.NDArray[np.float64]:
-        return self.angular_velocity
-
-    def get_control_torque(self) -> npt.NDArray[float64]:
-        return self.control_torque
-
+@dataclass
 class ReactionWheelAction(ActuatorAction):
-    pass
+    goal_angular_velocity: NDArray[np.float64]
 
 
 class SetOfReactionWheels(Actuator[SetOfReactionWheelsState, ReactionWheelAction]):
     def __init__(self, state: SetOfReactionWheelsState) -> None:
         self._state = state
-        self.max_torque = 1
-        self.max_angular_velocity = 1
-        self.intertia = 1
-        self.max_angular_accel_approx = 1
 
     @property
     def state(self) -> SetOfReactionWheelsState:
@@ -49,31 +33,5 @@ class SetOfReactionWheels(Actuator[SetOfReactionWheelsState, ReactionWheelAction
             raise Exception("Wrong State")
         self._state = state
 
-    def actuate(self, control_torque: npt.NDArray[float64], dt: float, state: SetOfReactionWheelsState) -> SetOfReactionWheelsState:
-        # check limits of torques
-        for i in range(0, 2):
-            if abs(control_torque[i]) > self.max_torque:
-                control_torque[i] = np.sign(control_torque[i]) * self.max_torque
-
-        # computing angular acceleration
-        α = control_torque * (1 / self.intertia)
-
-        # computing desired angular velocity
-        current_ω = self.state.get_angular_velocity()
-
-        goal_ω = current_ω + (α * dt)
-
-        # check saturation in the angular velocity,and change the
-        # torque accordingly
-        for i in range(0, 3):
-            if abs(goal_ω[i]) > self.max_angular_velocity:
-                control_torque[i] = np.sign(
-                    control_torque[i]) * (self.max_angular_velocity - abs(current_ω[i])) * self.intertia / dt
-                goal_ω[i] = self.max_angular_velocity * np.sign(goal_ω)
-
-        if abs(np.linalg.norm(current_ω)) - abs(np.linalg.norm(goal_ω)) > (math.sqrt(4 * self.max_angular_accel_approx* self.max_angular_accel_approx) * dt):
-            raise Exception("Error in the processing of angular velocity")
-
-        # updating instantaneous torque and the angular velocity
-        self._state = SetOfReactionWheelsState(control_torque, goal_ω, self.intertia)
-        return self._state
+    def actuate(self, action: ReactionWheelAction) -> None:
+        pass
